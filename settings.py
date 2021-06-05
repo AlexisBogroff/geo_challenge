2a3a060d90764bab73ce343dc503d61d47df7012
AlexisBogroff <alexis.bogroff@gmail.com>
2021-06-05 02:18:25 +0200

    Add smooth and alert_fld paths settings

--- settings.py
+++ settings.py
@@ -1,5 +1,17 @@
 """
 Settings for geo challenge alert system
 """
+PATH_ALERTS = 'data/alerts/'
+PATH_DATA_TOT = 'data/starships_clean.csv'
 PATH_DATA_INIT = 'data/starships_slice_init.csv'
-PATH_DATA_NEW = 'data/starships_slice_new_1.csv'
+PATH_DATA_NEW_1 = 'data/starships_slice_new_1.csv'
+PATH_DATA_NEW_2 = 'data/starships_slice_new_2.csv'
+PATH_DATA_SNAP_1 = 'data/starships_snap_1.csv'
+PATH_DATA_SNAP_2 = 'data/starships_snap_2.csv'
+
+# Reduct noise by lowering the precision
+SMOOTH = 5
+
+# Data range to use [date_min, date_max[
+DATE_MIN = '2020-01-01'
+DATE_MAX = '2020-10-01'
