fae07d333539062028fbecad6686f8168f4ac78f
AlexisBogroff <alexis.bogroff@gmail.com>
2021-06-05 02:19:16 +0200

    Add date filter, data smoothness, general static detection

--- alert_system.py
+++ alert_system.py
@@ -1,27 +1,28 @@
 """
-Raise alerts on sensible ratios
+Raise alerts on sensible metrics
 
 Alerts:
-- large variation of ships (by type)
-- report static ships
-- rare appearance of a ship (if a droids is detected)
-- new areas organization (variation num clusters, shift in localisation)
+- variation of ships count (by type)
+- static ships
+- (not implemented) rare appearance of a ship (if a droids is detected)
+- (not implemented) new areas organization (evol num clusters, shift position)
 """
-from settings import PATH_DATA_INIT, PATH_DATA_NEW
+import settings as stng
 from fcts import Detector, load_data
 
 # Load data
-data_t0 = load_data(PATH_DATA_INIT)
-data_t1 = load_data(PATH_DATA_NEW)
+data_t0 = load_data(stng.PATH_DATA_TOT, date_max=stng.DATE_MIN)
+data_t1 = load_data(stng.PATH_DATA_TOT, date_min=stng.DATE_MIN, date_max=stng.DATE_MAX)
+# data_t0 = load_data(stng.PATH_DATA_SNAP_1)
+# data_t1 = load_data(stng.PATH_DATA_SNAP_2)
 
 # Init detector
 detector = Detector(data_t0, data_t1)
 
 # Run scans
 detector.detect_large_variations(threshold=1.7)
-# static_objects = detector.detect_static_objects()
-# rare_objects = detector.detect_rare_objects()
-# area_change = detector.detect_area_change()
+detector.detect_static_objects(n_periods_min=1, smooth=5)
+detector.detect_static_objects_2_snaps(smooth=3)
 
 # Save alerts to file
-detector.export()
+detector.export(stng.PATH_ALERTS)
--- fcts.py
+++ fcts.py
@@ -13,6 +13,7 @@ class Detector:
     def __init__(self, data_t0, data_t1):
         self.data_t0 = data_t0
         self.data_t1 = data_t1
+        self.static_ships = None
         self.count_outliers = pd.DataFrame(columns=['date',
                                                     'ship_type',
                                                     'count',
@@ -86,20 +87,88 @@ class Detector:
             self._extract_breach(counts_new, stats, ship, 'bound_max')
 
         if verbose:
+            print('\n\nOuliters count:\n')
             print(self.count_outliers)
 
 
-    def detect_static_objects(self, verbose=True):
+    def detect_static_objects(self, n_periods_min=1, smooth=None, verbose=True):
         """
-        Detect static objects
+        Detect static objects on the given period
 
         Args:
-            verbose (bool): activate verbosity
+            n_periods: number of inter periods. For example:
+                       - n_periods_min=1: detects all static ships (static
+                                          over any duration).
+                       - n_periods_min=2: detects ships that were static for
+                                          at least 2 periods, i.e. 3 dates.
+            smooth (int): precision to round the latitude and longitude
+            verbose (bool): display results
 
         Returns:
             Static objects ordered by duration
+
+        Note:
+            Be careful as a low precision might consider different ships as
+            a unique ship. Thus, it might consider that a ship has been static
+            over multiple periods, althought the comparison is made btw 2 snaps.
+            To keep information on the date, use the more restrictive function
+            detect_static_objects_2_snaps.
         """
-        raise NotImplementedError
+        AGG_COLS = ['lon', 'lat', 'ship_type']
+
+        # Merge data
+        data = pd.concat([self.data_t0, self.data_t1], ignore_index=True)
+
+        if smooth:
+            data = smooth_positions(data, smooth)
+
+        # Mask static rows
+        ma_duplicated_pos = data[AGG_COLS].duplicated()
+
+        # Count number of static periods by static ship
+        static = data[ma_duplicated_pos][AGG_COLS].value_counts()
+
+        # Filter on duration
+        ma_duration = static >= n_periods_min
+        static = static[ma_duration]
+
+        # Set to attribute
+        self.static_ships = static
+
+        if verbose:
+            print(f'\n\nStatic ships over at least {n_periods_min} periods:\n')
+            print(static)
+
+
+    def detect_static_objects_2_snaps(self, smooth=None, verbose=True):
+        """
+        Detect static objects between two snapshots
+
+        Args:
+            smooth (int): precision to round the latitude and longitude
+            verbose (bool): display results
+
+        Returns:
+            Static objects ordered by duration
+
+        Note:
+            Do not use on more than two snapshots at a time, as results
+            might not be as expected. Idem when using low precision (smooth).
+        """
+        data_t0 = self.data_t0.copy()
+        data_t1 = self.data_t1.copy()
+
+        if smooth:
+            data_t0 = smooth_positions(data_t0, threshold=smooth)
+            data_t1 = smooth_positions(data_t1, threshold=smooth)
+
+        # Get static ships
+        static = pd.merge(data_t0, data_t1, how='inner', on=['lon', 'lat'])
+        self.static_ships = static
+
+        if verbose:
+            print('\n\nStatic ships between 2 periods:\n')
+            print(static)
 
 
     def detect_rare_objects(self, threshold=10, verbose=True):
@@ -116,7 +185,7 @@ class Detector:
         raise NotImplementedError
 
 
-    def export(self, f_name='alerts', ext='csv'):
+    def export(self, f_name='alerts', ext='csv', outliers=True, static=True):
         """
         Save data to csv file
 
@@ -129,12 +198,21 @@ class Detector:
             Only export count_outliers for now. Others not yet implemented.
         """
         f_name += '_' + datetime.now().strftime("%Y%m%d_%H%M%S")
-        self.count_outliers.sort_values('date', ascending=False, inplace=True)
-        self.count_outliers.to_csv(f_name + '.' + ext)
+
+        if outliers:
+            # Export outliers
+            f_name_outliers = f_name + '_outliers'
+            self.count_outliers.sort_values('date', ascending=False, inplace=True)
+            self.count_outliers.to_csv(f_name_outliers + '.' + ext)
+
+        if static:
+            # Export static ships:
+            f_name_static = f_name + '_static'
+            self.static_ships.to_csv(f_name_static + '.' + ext)
 
 
 
-def load_data(path, idx_col='id', date_col='date', verbose=True):
+def load_data(path, idx_col='id', date_col='date', verbose=True, **kwargs):
     """
     Load data
 
@@ -143,9 +221,40 @@ def load_data(path, idx_col='id', date_col='date', verbose=True):
         idx_col (str): column to use as index
         date_col (str): column to cast as datetime
         verbose (bool): activate verbosity
+        kwargs: date parameters of filter_on_date
 
     Returns:
         data (DataFrame)
     """
     data = pd.read_csv(path, index_col=idx_col, parse_dates=[date_col])
+    data = filter_on_date(data, date_col, **kwargs)
     return data
+
+
+def filter_on_date(data, date_col, date_min=None, date_max=None):
+    """
+    Extract data corresponding to specified date range
+
+    Args:
+        data (DataFrame)
+        date_col: name of column with dates
+        date_min (str or datetime): min date included
+        date_max (str or datetime): max date excluded
+
+    Returns:
+        data filtered
+    """
+    if date_min:
+        ma_min = data[date_col] >= date_min
+        data = data[ma_min]
+    if date_max:
+        ma_max = data[date_col] < date_max
+        data = data[ma_max]
+    return data
+
+
+def smooth_positions(data, threshold):
+    smooth = data.copy()
+    smooth['lon'] = round(smooth['lon'], threshold)
+    smooth['lat'] = round(smooth['lat'], threshold)
+    return smooth
